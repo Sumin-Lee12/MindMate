42d3253eae6a58ebcedea19837cfeb1c4c5d93f7
K-jisu <wltn7star@naver.com>
2025-06-25 03:54:00 +0900

    style : css 수정 (#2)

--- src/components/ui/search-input.tsx
+++ src/components/ui/search-input.tsx
@@ -2,7 +2,10 @@ import { Image, TextInput, View } from 'react-native';
 
 const SearchInput = () => {
   return (
-    <View className="w-full flex-row items-center rounded-full bg-white px-4">
+    <View className="w-full flex-row items-center rounded-xl border bg-white px-4">
+      /**Todo
+        * 1. Search icon을 나중에 변경
+       */
       <Image source={require('../../../assets/icons/search-icon.png')} />
       <TextInput className="h-[52px] flex-1" />
     </View>
