dbd7503f13a448c77ea3ff2ecd488aaf7db0a884
K-jisu <wltn7star@naver.com>
2025-06-25 04:01:12 +0900

    fix : 주석 수정 (#2)

--- src/components/ui/search-input.tsx
+++ src/components/ui/search-input.tsx
@@ -3,9 +3,9 @@ import { Image, TextInput, View } from 'react-native';
 const SearchInput = () => {
   return (
     <View className="w-full flex-row items-center rounded-xl border bg-white px-4">
-      /**Todo
-        * 1. Search icon을 나중에 변경
-       */
+      {/* Todo
+       * 1. Search icon을 나중에 변경
+       */}
       <Image source={require('../../../assets/icons/search-icon.png')} />
       <TextInput className="h-[52px] flex-1" />
     </View>
