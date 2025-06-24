6e68b2c3d09851e62f5980f9784378246c14204c
K-jisu <wltn7star@naver.com>
2025-06-24 23:42:31 +0900

    add : search category 상수 파일 추가 (#2)

---
+++ src/features/constants/search-category-constants.tsx
@@ -0,0 +1,6 @@
+export const searchCategories = [
+  { icon: '🗝️', label: '개인용품', color: 'foggyBlue' },
+  { icon: '📱', label: '모바일', color: 'paleYellow' },
+  { icon: '💻', label: '전자제품', color: 'pink' },
+  { icon: '☕', label: '주방용품', color: 'teal' },
+];
