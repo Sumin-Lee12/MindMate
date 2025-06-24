a8cc27cf30cecae9cf9621bad0b5c4f0daf59e58
K-jisu <wltn7star@naver.com>
2025-06-24 23:43:11 +0900

    refactor : search category button 수정 (#2)

--- src/features/search/components/search-category-button.tsx
+++ src/features/search/components/search-category-button.tsx
@@ -2,19 +2,35 @@ import React from 'react';
 import { Text, TouchableOpacity } from 'react-native';
 
 type SearchCategoryButtonProps = {
-  category: {
-    icon: React.ReactNode;
-    label?: string;
-    color: string;
-  };
+  label: string;
 };
 
-const SearchCategoryButton = ({ category }: SearchCategoryButtonProps) => {
+type LabelColorMap = '개인용품' | '모바일' | '전자제품' | '주방용품';
+
+const SearchCategoryButton = ({ label }: SearchCategoryButtonProps) => {
+  const labelColorMap: Record<LabelColorMap, string> = {
+    개인용품: 'foggyBlue',
+    모바일: 'paleYellow',
+    전자제품: 'pink',
+    주방용품: 'teal',
+  };
+
+  const color = labelColorMap[label as LabelColorMap] ?? 'foggyBlue';
+
+  const iconMap: Record<LabelColorMap, string> = {
+    개인용품: '🗝️',
+    모바일: '📱',
+    전자제품: '💻',
+    주방용품: '☕',
+  };
+
+  const icon = iconMap[label as LabelColorMap] ?? '🫠';
+
   return (
     <TouchableOpacity
-      className={`h-[50px] w-[50px] items-center justify-center rounded-xl bg-${category.color}`}
+      className={`h-[50px] w-[50px] items-center justify-center rounded-xl bg-${color}`}
     >
-      <Text>{category.icon}</Text>
+      <Text>{icon}</Text>
     </TouchableOpacity>
   );
 };
