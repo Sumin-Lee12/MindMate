f6741b0f1bb63fbf92bc36c3d5d0a4efdb6017dc
K-jisu <wltn7star@naver.com>
2025-06-25 14:04:10 +0900

    feat : 카테고리의 아이콘과 색상을 변환하는 함수 생성 (#2)

---
+++ src/features/search/utils/getCategoryData.tsx
@@ -0,0 +1,14 @@
+import { searchCategories } from '../constants/search-category-constants';
+
+/**
+ * 카테고리의 아이콘과 색상을 반환하는 함수
+ * @param label - 카테고리의 라벨
+ * @returns { icon: string, color: string }
+ */
+export const getCategoryData = (label: string) => {
+  const foundCategory = searchCategories.find((category) => category.label === label);
+  return {
+    icon: foundCategory?.icon ?? '🫠',
+    color: foundCategory?.color ?? 'foggyBlue',
+  };
+};
