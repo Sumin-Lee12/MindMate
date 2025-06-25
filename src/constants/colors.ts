0193b864076946e338d8a5065002760f96128319
Sumin-Lee12 <leesumin041@gmail.com>
2025-06-25 19:35:10 +0900

    add: 아이콘용 색상 변수 생성 (constants 폴더)

---
+++ src/constants/colors.ts
@@ -0,0 +1,14 @@
+// 아이콘 색상 수정을 위한 색상 변수입니다.
+
+export const Colors = {
+  black: '#000000',
+  gray: '#7D7D7D',
+  white: '#ffffff',
+  teal: '#C9EFEF',
+  paleYellow: '#FFE5BC',
+  pink: '#FFD7DD',
+  turquoise: '#F0F3FF',
+  foggyBlue: '#BDC7FF',
+  paleCobalt: '#576BCD',
+  red: '#FF4848',
+};
