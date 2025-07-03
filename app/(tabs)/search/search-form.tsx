29f1ca8170494a5418f84e074843d78e794723f6
K-jisu <wltn7star@naver.com>
2025-07-04 03:02:57 +0900

    feat: search 의 폼 제출 페이지 레이아웃 구성 (#14)

--- app/(tabs)/search/search-form.tsx
+++ app/(tabs)/search/search-form.tsx
@@ -1,74 +1,110 @@
-import { Text, TextInput, View } from 'react-native';
-import { useEffect, useState } from 'react';
-import Button from '../../../src/components/ui/button';
-// import { createDb } from '../../../src/lib/db/db';
+import {
+  Keyboard,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+  Text,
+  TouchableWithoutFeedback,
+  View,
+} from 'react-native';
+import Button from '@/src/components/ui/button';
+import { useState } from 'react';
+import SearchLabelInput from '@/src/features/search/components/search-label-input';
+import SearchCategoryPicker from '@/src/features/search/components/search-category-picker';
+import ImageAddButton from '@/src/components/ui/image-button';
 
 const SearchForm = () => {
   const [name, setName] = useState('');
-  const [category, setCategory] = useState('');
   const [location, setLocation] = useState('');
   const [detailLocation, setDetailLocation] = useState('');
 
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     await createDb();
-  //   };
-  //   fetchData();
-  // }, []);
+  // dropdown 상태
+  const [open, setOpen] = useState(false);
+  const [category, setCategory] = useState(null);
+  const [items, setItems] = useState([
+    { label: '개인용품', value: 'personal' },
+    { label: '모바일', value: 'mobile' },
+    { label: '전자기기', value: 'electronics' },
+    { label: '주방용품', value: 'kitchen' },
+  ]);
 
-  // const handleSubmit = async () => {
-  //   const MindMateDb = await createDb();
-  //   await MindMateDb.runAsync(
-  //     `
-  //       INSERT INTO items (name, category, location, detail_location)
-  //       VALUES (?, ?, ?, ?)
-  //     `,
-  //     [name, category, location, detailLocation],
-  //   );
+  const handleSubmit = () => {
+    const formData = { name, category, location, detailLocation };
+    clenarSearchForm();
+    console.log('Form Data:', formData);
+  };
 
-  //   console.log(await MindMateDb.getAllAsync('SELECT * FROM items'));
-  // };
+  // 폼 초기화 함수
+  const clenarSearchForm = () => {
+    setName('');
+    setLocation('');
+    setDetailLocation('');
+    setCategory(null);
+    setOpen(false);
+    setItems([
+      { label: '개인용품', value: 'personal' },
+      { label: '모바일', value: 'mobile' },
+      { label: '전자기기', value: 'electronics' },
+      { label: '주방용품', value: 'kitchen' },
+    ]);
+  };
 
-  // const handleDelete = async () => {
-  //   const MindMateDb = await createDb();
-  //   await MindMateDb.runAsync('DELETE FROM items');
-
-  //   console.log(await MindMateDb.getAllAsync('SELECT * FROM items'));
-  // };
+  const handleAddImage = () => {
+    console.log('Image button pressed');
+  };
 
   return (
-    <View className="h-full flex-1 items-center justify-center bg-white">
-      <TextInput
-        className="border-gray-300 mb-4 h-12 w-3/4 rounded-lg border px-4"
-        placeholder="물건 이름"
-        value={name}
-        onChangeText={setName}
-      />
-      <TextInput
-        className="border-gray-300 mb-4 h-12 w-3/4 rounded-lg border px-4"
-        placeholder="카테고리"
-        value={category}
-        onChangeText={setCategory}
-      />
-      <TextInput
-        className="border-gray-300 mb-4 h-12 w-3/4 rounded-lg border px-4"
-        placeholder="간략한 위치"
-        value={location}
-        onChangeText={setLocation}
-      />
-      <TextInput
-        className="border-gray-300 mb-4 h-12 w-3/4 rounded-lg border px-4"
-        placeholder="상세 위치"
-        value={detailLocation}
-        onChangeText={setDetailLocation}
-      />
-      <Button className="w-3/4 rounded-lg bg-blue-500 px-4 py-2" onPress={handleSubmit}>
-        <Text>제출</Text>
-      </Button>
-      <Button className="w-3/4 rounded-lg bg-blue-500 px-4 py-2" onPress={handleDelete}>
-        <Text>전체 삭제</Text>
-      </Button>
-    </View>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      className="flex-1"
+    >
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <ScrollView contentContainerStyle={{ flexGrow: 1 }} keyboardShouldPersistTaps="handled">
+          <View className="flex-1 items-center justify-around bg-turquoise p-4">
+            <SearchLabelInput
+              label="물건 이름"
+              placeholder="물건 이름을 입력해 주세요"
+              value={name}
+              onChangeText={setName}
+            />
+
+            <View className="z-10 mb-7 h-20 w-full items-start justify-center gap-2">
+              <Text className="text-md text-paleCobalt">카테고리</Text>
+              <SearchCategoryPicker
+                open={open}
+                value={category}
+                items={items}
+                setOpen={setOpen}
+                setValue={setCategory}
+                setItems={setItems}
+              />
+            </View>
+
+            <SearchLabelInput
+              label="간략한 위치"
+              placeholder="간략한 위치를 입력해 주세요"
+              value={location}
+              onChangeText={setLocation}
+            />
+
+            <SearchLabelInput
+              label="상세 위치"
+              placeholder="상세 위치를 입력해 주세요"
+              value={detailLocation}
+              onChangeText={setDetailLocation}
+              className="h-32"
+              multiline
+            />
+
+            <ImageAddButton onPress={handleAddImage} />
+
+            <Button className="w-3/4 rounded-lg bg-blue-500 px-4 py-2" onPress={handleSubmit}>
+              <Text className="text-center text-lg text-white">등록하기</Text>
+            </Button>
+          </View>
+        </ScrollView>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
   );
 };
 
