2df2473d22cbfad3c017a7dff4b0ac37ba381fcd
mbdyjk@gmail.com <mbdyjk@gmail.com>
2025-07-04 11:49:09 +0900

    add: 추가 버튼 공통 컴포넌트 생성 및 적용 (#4)

--- app/(tabs)/routine/index.tsx
+++ app/(tabs)/routine/index.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import { View, Text, Button } from 'react-native';
+import AddButton from 'src/components/ui/add-button';
 import { useRouter } from 'expo-router';
 
 const RoutineMain = () => {
   const router = useRouter();
 
   return (
-    <View className="flex-1 items-center justify-center">
+    <View className="flex-1 items-center justify-center bg-white">
       <Text className="text-2xl font-bold">루틴 메인 페이지</Text>
       <Button title="루틴 생성" onPress={() => router.push('/routine/routineform')} />
       {/* 루틴 리스트 등 추가 예정 */}
+      <AddButton onPress={() => router.push('/routine/routineform')} />
     </View>
   );
 };
