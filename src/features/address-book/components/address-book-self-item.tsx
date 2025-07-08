725c28d48adeac99d7267c1bbb69fd425ca8222c
Taejin7088 <gog70888@gmail.com>
2025-07-09 02:21:11 +0900

    feat : 주소록 자기 정보 데이터 읽어와서 데이터 UI연결 (#20)

--- src/features/address-book/components/address-book-self-item.tsx
+++ src/features/address-book/components/address-book-self-item.tsx
@@ -2,19 +2,28 @@ import React from 'react';
 import { Text, TouchableOpacity, View } from 'react-native';
 import AddressBookName from './address-book-name';
 import AddressBookImage from './address-book-image';
+import { Contact } from '../types/address-book-type';
+import { useCallback } from 'react';
+import { getMyContact } from '../services/get-address-book-data';
+import { useAsyncDataGet } from '../../../hooks/use-async-data-get';
 
 const AddressBookSelfItem = () => {
+  const getMyContactUseCallBack = useCallback(getMyContact, []);
+  const { data } = useAsyncDataGet<Contact>(getMyContactUseCallBack);
+
+  if (!data) return null;
+
   return (
     <View className="flex-row justify-between px-4">
       <View className="flex items-center gap-4">
-        <AddressBookName>김이름</AddressBookName>
+        <AddressBookName>{data.name}</AddressBookName>
         <TouchableOpacity className="flex-row items-center justify-center rounded-lg bg-paleCobalt py-1">
           <Text className="px-9 text-md text-white">상세 정보 보기</Text>
         </TouchableOpacity>
       </View>
       <View className="item-center flex">
         <AddressBookImage />
-        <Text className="text-sm">010-0000-0000</Text>
+        <Text className="text-sm">{data.phone_number}</Text>
       </View>
     </View>
   );
