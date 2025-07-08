269a2e91e322fe1a0d1d7702b945b4bdaacb0309
K-jisu <wltn7star@naver.com>
2025-07-09 04:09:33 +0900

    refactor : 디자인에 맞게 수정, 함수 로직 분리, 이미지 추가 구현 (#23)

--- app/(tabs)/search/search-form.tsx
+++ app/(tabs)/search/search-form.tsx
@@ -1,4 +1,12 @@
-import { Keyboard, ScrollView, Text, TouchableWithoutFeedback, View } from 'react-native';
+import {
+  Keyboard,
+  ScrollView,
+  Text,
+  TouchableWithoutFeedback,
+  View,
+  KeyboardAvoidingView,
+  Platform,
+} from 'react-native';
 import Button from '@/src/components/ui/button';
 import { useState } from 'react';
 import SearchLabelInput from '@/src/features/search/components/search-label-input';
@@ -9,18 +17,16 @@ import { SearchCategoryLabel } from '@/src/features/search/db/search-db-types';
 import { Controller, useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useRouter } from 'expo-router';
-import * as ImagePicker from 'expo-image-picker';
 import { SearchFormSchema, searchFormSchema } from '@/src/features/search/utils/search-form-schema';
 import ImageButton from '@/src/components/ui/image-button';
-
-type ImageType = {
-  uri: string;
-  type: 'image' | 'video' | 'livePhoto' | 'pairedVideo' | undefined;
-};
+import { MediaType } from '@/src/types/common-db-types';
+import { insertMedia, pickMedia } from '@/src/lib/media-services';
+import { insertSearch } from '@/src/features/search/search-services';
 
 const SearchForm = () => {
   const router = useRouter();
-  const [images, setImages] = useState<ImageType[]>([]);
+  const [images, setImages] = useState<MediaType[]>([]);
+  const [id, setId] = useState(0);
 
   // dropdown 상태
   const [open, setOpen] = useState(false);
@@ -44,28 +50,16 @@ const SearchForm = () => {
     },
   });
 
+  // 폼 제출 함수
   const handleFormSubmit = async (data: SearchFormSchema) => {
     try {
-      const { lastInsertRowId } = await db.runAsync(
-        `
-        INSERT INTO search (name, category, location, description)
-        VALUES (?, ?, ?, ?)
-      `,
-        [data.name, data.category, data.location, data.description ?? null],
-      );
-      if (images.length > 0) {
-        await Promise.all(
-          images.map((image) =>
-            db.runAsync(
-              `
-            INSERT INTO media (owner_type, owner_id, media_type, file_path)
-            VALUES (?, ?, ?, ?)
-          `,
-              ['search', lastInsertRowId, image.type ?? null, image.uri],
-            ),
-          ),
-        );
-      }
+      await db.withTransactionAsync(async () => {
+        const lastInsertRowId = await insertSearch(data);
+        setId(lastInsertRowId);
+        if (images.length > 0) {
+          await insertMedia(images, 'search', id);
+        }
+      });
       router.back();
     } catch (error) {
       console.error('폼 제출 오류:', error);
@@ -73,28 +67,11 @@ const SearchForm = () => {
     }
   };
 
+  // 이미지 추가 함수
   const handleAddImage = async () => {
-    const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
-    if (status !== 'granted') {
-      alert('시스템 설정에서 갤러리 접근 권한을 허용해 주세요.');
-      return;
-    }
-    try {
-      const result = await ImagePicker.launchImageLibraryAsync({
-        mediaTypes: ['images', 'videos', 'livePhotos'],
-        allowsEditing: true,
-        aspect: [4, 3],
-        quality: 1,
-      });
-      if (!result.canceled) {
-        const uri = result.assets[0].uri;
-        const type = result.assets[0].type;
-        const newImage = { uri, type };
-        setImages((prev) => [...prev, newImage]);
-      }
-    } catch (error) {
-      alert('이미지 업로드 에러' + error);
-    }
+    const newImage = await pickMedia();
+    if (!newImage) return;
+    setImages((prev) => [...prev, newImage]);
   };
 
   // 이미지 삭제
@@ -104,96 +81,108 @@ const SearchForm = () => {
 
   return (
     <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-      <ScrollView contentContainerStyle={{ flexGrow: 1 }} keyboardShouldPersistTaps="handled">
-        <View className="flex-1 justify-between bg-turquoise px-4">
-          <View className="mt-8 flex-1 items-center">
-            <Controller
-              control={control}
-              name="name"
-              render={({ field: { onChange, value, onBlur } }) => (
-                <SearchLabelInput
-                  label="물건 이름"
-                  placeholder="물건 이름을 입력해 주세요"
-                  value={value}
-                  onChangeText={onChange}
-                  onBlur={onBlur}
-                  errors={errors.name?.message}
-                />
-              )}
-            />
-            <View className={`z-10 my-5 h-20 w-full items-start justify-center gap-2`}>
-              <Text className="text-md text-paleCobalt">카테고리</Text>
-              <Controller
-                name="category"
-                control={control}
-                render={({ field: { onChange, value } }) => (
-                  <SearchCategoryPicker
-                    open={open}
-                    value={value}
-                    items={items}
-                    setOpen={setOpen}
-                    setValue={onChange}
-                    setItems={setItems}
-                    onChangeValue={(val) => onChange(val)}
-                  />
-                )}
-              />
-              {errors.category && (
-                <Text className="text-ss text-red-500">{errors.category.message}</Text>
-              )}
-            </View>
-            <View className="w-full">
+      <View className="flex-1 bg-turquoise px-4 pt-8">
+        <KeyboardAvoidingView
+          behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+          keyboardVerticalOffset={Platform.OS === 'ios' ? 80 : 0}
+        >
+          <ScrollView contentContainerStyle={{ flexGrow: 1 }} keyboardShouldPersistTaps="handled">
+            <View className="items-center">
               <Controller
                 control={control}
-                name="location"
+                name="name"
                 render={({ field: { onChange, value, onBlur } }) => (
                   <SearchLabelInput
-                    label="간략한 위치"
-                    placeholder="간략한 위치를 입력해 주세요"
+                    label="물건 이름"
+                    placeholder="물건 이름을 입력해 주세요"
                     value={value}
                     onChangeText={onChange}
                     onBlur={onBlur}
-                    errors={errors.location?.message}
+                    errors={errors.name?.message}
                   />
                 )}
               />
-            </View>
-            <Controller
-              control={control}
-              name="description"
-              render={({ field: { onChange, value } }) => (
-                <SearchLabelInput
-                  label="상세 위치"
-                  placeholder="상세 위치를 입력해 주세요"
-                  value={value ?? ''}
-                  onChangeText={onChange}
-                  className="my-5 h-32"
-                  multiline
+              <View className={`z-10 mb-9 w-full items-start justify-center gap-2`}>
+                <Text className="text-lg text-paleCobalt">카테고리</Text>
+                <Controller
+                  name="category"
+                  control={control}
+                  render={({ field: { onChange, value } }) => (
+                    <SearchCategoryPicker
+                      open={open}
+                      value={value}
+                      items={items}
+                      setOpen={setOpen}
+                      setValue={onChange}
+                      setItems={setItems}
+                      onPress={Keyboard.dismiss}
+                      onChangeValue={(val) => onChange(val)}
+                    />
+                  )}
                 />
-              )}
-            />
+                <View className="h-5">
+                  {errors.category && (
+                    <Text className="text-ss text-red-500">{errors.category.message}</Text>
+                  )}
+                </View>
+              </View>
+              <View className="w-full">
+                <Controller
+                  control={control}
+                  name="location"
+                  render={({ field: { onChange, value, onBlur } }) => (
+                    <SearchLabelInput
+                      label="간략한 위치"
+                      placeholder="간략한 위치를 입력해 주세요"
+                      value={value}
+                      onChangeText={onChange}
+                      onBlur={onBlur}
+                      errors={errors.location?.message}
+                      className="my-0"
+                    />
+                  )}
+                />
+              </View>
+              <Controller
+                control={control}
+                name="description"
+                render={({ field: { onChange, value } }) => (
+                  <SearchLabelInput
+                    label="상세 위치"
+                    placeholder="상세 위치를 입력해 주세요"
+                    value={value ?? ''}
+                    onChangeText={onChange}
+                    className="my-0"
+                    multiline
+                  />
+                )}
+              />
 
-            <View className="h-28 w-full items-start justify-center gap-2">
-              <Text className="text-md text-paleCobalt">사진</Text>
-              <View className="flex flex-row gap-2">
-                {images.map((image, idx) => (
-                  <ImageButton key={idx} onPress={() => handleRemoveImage(idx)} image={image.uri} />
-                ))}
-                {images.length < 3 && <ImageButton onPress={handleAddImage} />}
+              <View className="mb-8 h-28 w-full items-start justify-center gap-2">
+                <Text className="text-lg text-paleCobalt">사진 추가</Text>
+                <View className="flex flex-row gap-2">
+                  {images.map((image, idx) => (
+                    <ImageButton
+                      key={idx}
+                      onPress={() => handleRemoveImage(idx)}
+                      image={image.uri}
+                    />
+                  ))}
+                  {images.length < 3 && <ImageButton onPress={handleAddImage} />}
+                </View>
               </View>
             </View>
-          </View>
-
-          <View className="w-full flex-1 items-center">
-            <Button
-              className="w-3/4 rounded-lg bg-blue-500 px-4 py-2"
-              onPress={handleSubmit(handleFormSubmit)}
-            >
-              <Text className="text-center text-lg text-white">등록하기</Text>
-            </Button>
-          </View>
-        </View>
-      </ScrollView>
+            <View className="w-full items-center bg-turquoise pb-6">
+              <Button
+                className="h-[50px] w-3/4 rounded-xl bg-paleCobalt "
+                onPress={handleSubmit(handleFormSubmit)}
+              >
+                <Text className="text-center text-lg text-white">등록하기</Text>
+              </Button>
+            </View>
+          </ScrollView>
+        </KeyboardAvoidingView>
+      </View>
     </TouchableWithoutFeedback>
   );
 };
