bc008e579827d6884f60361e54cc3b08062cae33
K-jisu <wltn7star@naver.com>
2025-07-09 10:18:40 +0900

    feat : search 테이블 데이터 불러오기 (#28)

--- app/(tabs)/search/index.tsx
+++ app/(tabs)/search/index.tsx
@@ -1,15 +1,43 @@
-import { Text, TouchableOpacity, View } from 'react-native';
-import SearchInput from '../../../src/components/ui/search-input';
-import SearchCategoryButton from '../../../src/features/search/components/search-category-button';
-import SearchItemCard from '../../../src/features/search/components/search-item-card';
-import { searchCategories } from '../../../src/features/search/constants/search-category-constants';
-import { useRouter } from 'expo-router';
+import { ScrollView, Text, TouchableOpacity, View } from 'react-native';
+import SearchInput from '@/src/components/ui/search-input';
+import SearchCategoryButton from '@/src/features/search/components/search-category-button';
+import SearchItemCard from '@/src/features/search/components/search-item-card';
+import { searchCategories } from '@/src/features/search/constants/search-category-constants';
+import { useFocusEffect, useRouter } from 'expo-router';
+import { useCallback, useState } from 'react';
+import { db } from '@/src/hooks/use-initialize-database';
+import { SearchData } from '@/src/features/search/db/search-db-types';
+import Button from '@/src/components/ui/button';
 
 const HomeScreen = () => {
+  const [items, setItems] = useState<SearchData[]>([]);
   const router = useRouter();
 
   const handleCreateItem = () => {
     router.push('/(tabs)/search/search-form');
+  };
+
+  useFocusEffect(
+    useCallback(() => {
+      getSearchItems();
+    }, []),
+  );
+
+  const getSearchItems = async () => {
+    const items = (await db.getAllAsync(`SELECT * FROM search`)) as SearchData[];
+    const data = await db.getAllAsync(`SELECT * FROM media`);
+    console.log(data);
+    setItems(items);
+    // console.log('Search items:', items);
+  };
+
+  const resetTable = async () => {
+    await db.runAsync(`
+      DELETE FROM search
+    `);
+    await db.runAsync(`
+      DELETE FROM media
+    `);
   };
 
   return (
@@ -24,15 +52,22 @@ const HomeScreen = () => {
           <SearchCategoryButton key={index} label={category.label} />
         ))}
       </View>
-
-      {/**Todo
-       * 추후 저장된 데이터를 이용하여 목록 보여주기
-       */}
-      <View className="w-full gap-4">
-        {Array.from({ length: 4 }).map((_, index) => (
-          <SearchItemCard id={String(index)} key={index} />
-        ))}
-      </View>
+      <Button onPress={resetTable}>
+        <Text>초기화</Text>
+      </Button>
+      <ScrollView className="w-full">
+        <View className="w-full gap-4">
+          {items.map((item) => (
+            <SearchItemCard
+              key={item.id}
+              id={item.id}
+              category={item.category}
+              name={item.name}
+              location={item.location}
+            />
+          ))}
+        </View>
+      </ScrollView>
 
       <TouchableOpacity
         className="absolute bottom-8 right-6 h-16 w-16 items-center justify-center rounded-full bg-paleCobalt"
