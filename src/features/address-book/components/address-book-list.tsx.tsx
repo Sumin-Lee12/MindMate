391cee6563e229f9f8973c30c7361fbf2783e76e
Taejin7088 <gog70888@gmail.com>
2025-07-10 04:22:27 +0900

    style : 리스트 버튼, 마지막 요소 마진 추가 (#20)

--- src/features/address-book/components/address-book-list.tsx.tsx
+++ src/features/address-book/components/address-book-list.tsx.tsx
@@ -1,20 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { ScrollView, View } from 'react-native';
 import AddressBookItem from './address-book-item';
 import { Contact } from '../types/address-book-type';
 import { getOthersContacts } from '../services/get-address-book-data';
 import { useCallback } from 'react';
 import { useAsyncDataGet } from '../../../hooks/use-async-data-get';
+import { useFocusPage } from '@/src/hooks/use-focus-page';
 
 const AddressBookList = () => {
   const getOthersContactsUseCallBack = useCallback(getOthersContacts, []);
-  const { data } = useAsyncDataGet<Contact[]>(getOthersContactsUseCallBack);
+  const { data, refetch } = useAsyncDataGet<Contact[]>(getOthersContactsUseCallBack);
+  useFocusPage(refetch);
 
   if (!data) return null;
 
   return (
-    <ScrollView className="h-full">
-      <View className="flex-col gap-2">
+    <ScrollView className="flex-1">
+      <View className="flex-col gap-2 last:mb-20">
         {data.map((contact) => {
           return <AddressBookItem key={contact.id} contact={contact} />;
         })}
