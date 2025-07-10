895c4424bacc5083089bdce527eb074a4c442d39
Taejin7088 <gog70888@gmail.com>
2025-07-10 16:06:20 +0900

    feat : 그룹아이템 변경 삭제하는 기능 추가 (#20)

--- src/features/address-book/components/contact-detail-group-section-list.tsx
+++ src/features/address-book/components/contact-detail-group-section-list.tsx
@@ -1,5 +1,5 @@
 import { useCallback, useEffect, useState } from 'react';
-import { ScrollView, Text, TextInput, View } from 'react-native';
+import { ScrollView, Text, TextInput, TouchableOpacity, View } from 'react-native';
 import { getNoteGroupsByContactId } from '../services/get-note-group-data';
 import { NoteGroup, NoteItem } from '../types/address-book-type';
 import { useAsyncDataGet } from '@/src/hooks/use-async-data-get';
@@ -8,7 +8,13 @@ import CommonBox from '@/src/components/ui/common-box';
 import Button from '@/src/components/ui/button';
 import BottomModal from '@/src/components/ui/bottom-modal';
 import { formTextStyle } from '../constants/style-class-constants';
-import { createNoteItem } from '../services/mutation-note-group-data';
+import {
+  createNoteItem,
+  deleteNoteItem,
+  updateNoteItem,
+} from '../services/mutation-note-group-data';
+import { EllipsisVertical } from 'lucide-react-native';
+import ActionMenu from './action-menu';
 
 const ContactDetailGroupSectionList = ({
   id,
@@ -45,35 +51,71 @@ const ContactDetailGroupList = ({ group }: { group: NoteGroup }) => {
   const [isModalVisible, setIsModalVisible] = useState(false);
   const { data, refetch } = useAsyncDataGet<NoteItem[]>(getNoteItemsByGroupIdCallback);
 
-  useEffect(() => {
-    refetch();
-  }, [group, isModalVisible]);
-
   return (
     <View className="gap-2 p-4">
       <Text className="mb-2 text-sm font-normal">{group.title}</Text>
       {data?.map((item) => {
-        return <ContactDetailGroupItem key={item.item_id} item={item} />;
+        return <ContactDetailGroupItem key={item.item_id} item={item} refetch={refetch} />;
       })}
       <AddContactDetailGroupItemButton onPress={() => setIsModalVisible(true)} />
       {isModalVisible && (
-        <AddContactDetailGroupItemModal
+        <EditContactDetailGroupItemModal
           isModalVisible={isModalVisible}
           setIsModalVisible={setIsModalVisible}
           groupId={group.group_id}
+          refetch={refetch}
         />
       )}
     </View>
   );
 };
 
-const ContactDetailGroupItem = ({ item }: { item: NoteItem }) => {
+const ContactDetailGroupItem = ({ item, refetch }: { item: NoteItem; refetch: () => void }) => {
+  const [isActionMenuVisible, setIsActionMenuVisible] = useState(false);
+  const [isEditModalVisible, setIsEditModalVisible] = useState(false);
+
+  const handleDeleteContactDetailGroupItem = async () => {
+    await deleteNoteItem(item.item_id.toString());
+    refetch();
+  };
+
+  //모달이 꺼졌을때 리패치 처리
+  useEffect(() => {
+    if (!isEditModalVisible) {
+      refetch();
+    }
+  }, [isEditModalVisible]);
+
   return (
     <CommonBox>
       <View className="gap-3">
+        <View className="flex-row justify-between">
+          <Text className="text-lg font-bold">{item.title}</Text>
+          <TouchableOpacity onPress={() => setIsActionMenuVisible(true)}>
+            <EllipsisVertical size={20} color="#666" />
+          </TouchableOpacity>
+        </View>
         <Text className="text-sm font-normal">{item.content}</Text>
-        <Text className="text-lg font-bold">{item.title}</Text>
       </View>
+      {isActionMenuVisible && (
+        <ActionMenu
+          isVisible={isActionMenuVisible}
+          onClose={() => setIsActionMenuVisible(false)}
+          onEdit={() => {
+            setIsEditModalVisible(true);
+          }}
+          onDelete={handleDeleteContactDetailGroupItem}
+        />
+      )}
+      {isEditModalVisible && (
+        <EditContactDetailGroupItemModal
+          isModalVisible={isEditModalVisible}
+          setIsModalVisible={setIsEditModalVisible}
+          groupId={item.group_id}
+          item={item}
+          refetch={refetch}
+        />
+      )}
     </CommonBox>
   );
 };
@@ -86,20 +128,34 @@ const AddContactDetailGroupItemButton = ({ onPress }: { onPress: () => void }) =
   );
 };
 
-const AddContactDetailGroupItemModal = ({
+const EditContactDetailGroupItemModal = ({
   isModalVisible,
   setIsModalVisible,
   groupId,
+  item,
+  refetch,
 }: {
   isModalVisible: boolean;
   setIsModalVisible: (visible: boolean) => void;
   groupId: number;
+  item?: NoteItem;
+  refetch: () => void;
 }) => {
-  const [title, setTitle] = useState('');
-  const [content, setContent] = useState('');
+  const [title, setTitle] = useState(item?.title || '');
+  const [content, setContent] = useState(item?.content || '');
 
-  const handleAddContactDetailGroupItem = async () => {
-    await createNoteItem(groupId.toString(), title, content);
+  const handleEditContactDetailGroupItem = async () => {
+    if (item) {
+      await updateNoteItem(item.item_id.toString(), {
+        group_id: item.group_id,
+        title,
+        content,
+      });
+      refetch();
+    } else {
+      await createNoteItem(groupId.toString(), title, content);
+      refetch();
+    }
   };
 
   return (
@@ -109,18 +165,18 @@ const AddContactDetailGroupItemModal = ({
           className={`text-md ${formTextStyle}`}
           value={title}
           onChangeText={setTitle}
-          placeholder="제목을 입력하세요"
+          placeholder={item?.title || '제목을 입력하세요'}
         />
         <TextInput
           className={`text-md ${formTextStyle}`}
           value={content}
           onChangeText={setContent}
-          placeholder="설명을 입력하세요"
+          placeholder={item?.content || '설명을 입력하세요'}
         />
       </View>
       <View className="w-full px-10 pb-4">
-        <Button onPress={handleAddContactDetailGroupItem}>
-          <Text>목록추가</Text>
+        <Button onPress={handleEditContactDetailGroupItem}>
+          <Text>{item ? '수정하기' : '목록추가'}</Text>
         </Button>
       </View>
     </BottomModal>
